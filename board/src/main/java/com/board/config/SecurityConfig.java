a49a0b052ba3ecb33727b1b5de19926eb4903e77
gyocheol <kys04171@naver.com>
2024-09-12 00:05:06 +0900

    feat : Spring Security 설정 끝 / 내일 회원가입, 로그인, 로그아웃 기능 개발

--- board/src/main/java/com/board/config/SecurityConfig.java
+++ board/src/main/java/com/board/config/SecurityConfig.java
@@ -1,9 +1,60 @@
 package com.board.config;
 
+import com.board.util.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig{
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()      // http basic 인증 방법 비활성화
+                .formLogin().disable()      // form 기반 로그인 비활성화
+                .csrf().disable()           // csrf 관련 설정 비활성화
+                .cors().disable()           // cors 관련 설정 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)     // 무상태 인증 방식인 JWT를 생성하고 서버에 보낼 때 세션을 생성하지 않도록 하는 것! JWT를 사용할 때 필수로 사용해야한다.
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()  // 현재는 모든 request 요청 혀용 TODO: 추후 권한 별 허용 예정
+                .and()
+                .addFilterBefore(jwtRequestFilter, BasicAuthenticationFilter.class);
+        return http.build();
+    }
+
+    /**
+     * 패스워드 인코더 설정
+     *
+     * @return BCryptPasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * login 시 사용자 유효성 검사 - 확장 가능성 높음 (JWT Filter로 직접 처리하면 아래 코드 삭제)
+     * @param authenticationConfiguration
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
---
+++ board/src/main/java/com/board/util/JwtRequestFilter.java
@@ -0,0 +1,58 @@
+package com.board.util;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private JwtUtil jwtUtil;
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        String jwt = null;
+        String username = null;
+
+        //HttpOnly 쿠키에서 JWT Token 가져오기
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie:cookies) {
+                if ("accessToken".equals(cookie.getName())) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (jwt != null) {
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.isTokenValid(jwt, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
