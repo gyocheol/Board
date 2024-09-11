2a16da7d46eb6eb1bbd14012738119295afa5425
gyocheol <kys04171@naver.com>
2024-09-11 19:01:07 +0900

    setting : MySQL 연동 완료 / 저녁 먹고 Security 적용하기

--- board/src/main/java/com/board/BoardApplication.java
+++ board/src/main/java/com/board/BoardApplication.java
@@ -2,12 +2,14 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "application, application-db, application-login, application-redis");
+		System.setProperty("spring.config.name", "application,application-db,application-login,application-redis");
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
--- board/src/main/java/com/board/entity/Member.java
+++ board/src/main/java/com/board/entity/Member.java
@@ -1,22 +1,31 @@
-//package com.board.entity;
-//
-//import lombok.Builder;
-//import lombok.Getter;
-//import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-//
-//import javax.persistence.*;
-// TODO : MySQL 연동 후 주석 풀기!
-//@Entity
-//@Getter
-//@Builder
-//public class Member {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String username;
-//
-//    @Column(nullable = false)
-//    private String password;
-//
-//}
+package com.board.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public void encodeStudentPassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
+}
